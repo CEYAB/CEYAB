76eff36f3568b5371d413c49004e20414d33b64f
Yacoubi <a.a.yacoubi@gmail.com>
2017-11-21 23:57:00 -0500

    login page(juste le login reste reset passwerot et register)

--- HouseHoldManager2/app/src/main/java/ca/uottawa/householdmanager/accountActivity/LoginActivity.java
+++ HouseHoldManager2/app/src/main/java/ca/uottawa/householdmanager/accountActivity/LoginActivity.java
@@ -1,13 +1,15 @@
-package ca.uottawa.householdmanager;
+package ca.uottawa.householdmanager.accountActivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+import ca.uottawa.householdmanager.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login2);
     }
 }
